aa0dbfdc168441169e73e8004c7f26f4bd37dcb6
Ayush Katre <ayushkatre1801@gmail.com>
2025-05-11 00:37:42 +0530

    V0 NOT WORKING WITH CURRENT JSON FORMAT

--- components/mdx/preview-content.tsx
+++ components/mdx/preview-content.tsx
@@ -12,7 +12,6 @@ import { Button } from "@/components/ui/button";
 import { ArrowUpRight, Copy, Check, CheckCheck, Terminal } from "lucide-react";
 import { copyComponent } from "@/lib/action";
 import { cn } from "@/lib/utils";
-import { OpenInV0Button } from "../open-in-v0-button";
 import { AnimatePresence, motion } from "motion/react";
 
 export default function PreviewContent({
@@ -170,7 +169,6 @@ export default function PreviewContent({
                     </a>
 
                     <div className="flex items-center gap-2">
-                        <OpenInV0Button name={openInV0()} />
                         <Button
                             ref={terminalButtonRef}
                             onClick={handleTerminalClick}
